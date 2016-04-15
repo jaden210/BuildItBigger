fdefe3c7dd96154f19142b4b455add1a27a38fb1
jaden <jaden210@gmail.com>
2016-04-15 10:37:05 -0600

    now with working android library

---
+++ jokeshower/src/main/java/com/example/jaden/jokeshower/JokeLibrary.java
@@ -0,0 +1,21 @@
+package com.example.jaden.jokeshower;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class JokeLibrary extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_joke_library);
+
+        Intent intent = getIntent();
+        String joke = intent.getStringExtra("joke");
+        TextView textView = (TextView) findViewById(R.id.joke);
+        textView.setText(joke);
+
+    }
+}
